8d8bdfaba94ea5a276e41022c21553977a004eeb
MrRobot <olivier.portal@laplateforme.io>
2025-05-12 13:58:34 +0200

    feat: job00 done

---
+++ Jour01/Job00.cpp
@@ -0,0 +1,51 @@
+//
+// Created by porta on 12/05/2025.
+//
+#include <iostream>
+
+/*int main()
+{
+	int x = 3;
+	int a = x +5;
+   	std::cout << a << std::endl;
+    return 0;
+}
+
+int main()
+{
+	int x;
+	int y = 3;
+	int a = x=y + 2;
+   	std::cout << a << std::endl;
+    return 0;
+}
+
+int main()
+{
+	int x = 5;
+	int y = 5;
+	int a = x==y;
+   	std::cout << a << std::endl;
+    return 0;
+}
+
+int main()
+{
+	int a = 9;
+	int b = 5;
+	int c = 6;
+	int d = 8;
+	a = a<b && c<d;
+   	std::cout << a << std::endl;
+    return 0;
+}*/
+
+int main()
+{
+	int i = 9;
+	int n = 5;
+	int p = 6;
+	int a = i++ * (n+p);
+   	std::cout << a << std::endl;
+    return 0;
+}
