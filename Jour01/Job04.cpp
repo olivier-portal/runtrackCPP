4a59dbf9a5a727942820a8620adf4cba5c159b20
MrRobot <olivier.portal@laplateforme.io>
2025-05-12 14:33:59 +0200

    feat: Job04 done

---
+++ Jour01/Job04.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <bits/ostream.tcc>
+//
+// Created by porta on 12/05/2025.
+//
+int add(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    std::cout << add(1, 5) << std::endl;
+    return 0;
+}
