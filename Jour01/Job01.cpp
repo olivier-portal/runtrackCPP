2987c5b34514fcefa79942896feba07c4d19ab32
MrRobot <olivier.portal@laplateforme.io>
2025-05-12 14:15:33 +0200

    feat: job01 done

---
+++ Jour01/Job01.cpp
@@ -0,0 +1,18 @@
+//
+// Created by porta on 12/05/2025.
+//
+#include <iostream>
+
+char c = '\x01';
+short int p = 10;
+
+int main()
+{
+    const int x = p + 3;
+    constexpr int c = 5;
+    const int y = c + 1;
+    const int z = p + c;
+    const int w = 3 * p + 5 * c;
+    std::cout << x << ", " << y << ", " << z << ", " << w << std::endl;
+    return 0;
+}
