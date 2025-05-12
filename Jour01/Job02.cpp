ab50a18cf4557712c571c1526143b43078cd775a
MrRobot <olivier.portal@laplateforme.io>
2025-05-12 14:27:22 +0200

    feat: job02 and job03 done

---
+++ Jour01/Job02.cpp
@@ -0,0 +1,13 @@
+//
+// Created by porta on 12/05/2025.
+//
+#include <iostream>
+
+char c = '\x01';
+short int p = 10;
+
+int main()
+{
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}
---
+++ Jour01/Job03.cpp
@@ -0,0 +1,18 @@
+//
+// Created by porta on 12/05/2025.
+//
+#include <iostream>
+
+char c = '\x01';
+short int p = 10;
+
+int main()
+{
+    int N = 10;
+    while (N != 0) {
+        N--;
+        std::cout << "Hello World" << std::endl;
+    }
+
+    return 0;
+}
