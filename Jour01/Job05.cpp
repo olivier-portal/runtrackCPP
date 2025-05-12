4ea34013d12314d75feb47aaecdff93c17a19ed1
MrRobot <olivier.portal@laplateforme.io>
2025-05-12 14:35:43 +0200

    feat: Job05 done

---
+++ Jour01/Job05.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <bits/ostream.tcc>
+//
+// Created by porta on 12/05/2025.
+//
+int multiply(int a, int b) {
+    return a * b;
+}
+
+int main() {
+    std::cout << multiply(2, 5) << std::endl;
+    return 0;
+}
