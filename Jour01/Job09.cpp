337c97814e57da20f19343b7e4353a409d5f2ea5
MrRobot <olivier.portal@laplateforme.io>
2025-05-13 11:35:22 +0200

    feat: Job09 done

---
+++ Jour01/Job09.cpp
@@ -0,0 +1,13 @@
+//
+// Created by porta on 13/05/2025.
+//
+#include <algorithm>
+#include <iostream>
+
+int main() {
+    int numbers[3];
+    std::cout << "Enter three integers: ";
+    std::cin >> numbers[0] >> numbers[1] >> numbers[2];
+    std::sort(numbers, numbers + 3);
+    std::cout << "The numbers from the smallest to the biggest are: " << numbers[0] << " " << numbers[1] << " " << numbers[2] << std::endl;
+}
