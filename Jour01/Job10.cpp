cb95747c2d9ff95265a1938c38e33ab37c1ec350
MrRobot <olivier.portal@laplateforme.io>
2025-05-13 13:30:49 +0200

    feat: Job10 done

--- Jour01/Job10.cpp
+++ Jour01/Job10.cpp
@@ -1,3 +1,20 @@
 //
 // Created by porta on 13/05/2025.
 //
+#include <iostream>
+
+double taxCalculator(const double price, const double quantity, const double tva) {
+        const double totalPrice = price * quantity * (tva / 100.0 + 1);
+        return totalPrice;
+}
+
+int main() {
+        double price, quantity, tva;
+        std::cout << "What is the price of your vegetables in euros?";
+        std::cin >> price;
+        std::cout << "What is the quantity of your vegetables in kg?";
+        std::cin >> quantity;
+        std::cout << "What is the tax cost of your vegetable?";
+        std::cin >> tva;
+        std::cout << "The total cost of your vegetable is: " << taxCalculator(price, quantity, tva) << " euros";
+}
