2d5e89a4fc6c7620bd475c7df615d904f025c7db
MrRobot <olivier.portal@laplateforme.io>
2025-05-14 10:10:14 +0200

    feat: Job02 done

---
+++ Jour02/Job02.cpp
@@ -0,0 +1,20 @@
+//
+// Created by porta on 14/05/2025.
+//
+#include <iostream>
+
+int main() {
+    int n;
+    std::cin >> n;
+    switch (n) {
+        case 0: std::cout << "Nul\n";
+        case 1:
+        case 2: std::cout << "Petit\n";
+            break;
+        case 3:
+        case 4:
+        case 5: std::cout << "Moyen\n";
+        default: std::cout << "Grand\n";
+    }
+    return 0;
+}
