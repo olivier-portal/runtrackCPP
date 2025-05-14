6d084cfd5b4df2adc4c29c73af42b5e3d2918b47
MrRobot <olivier.portal@laplateforme.io>
2025-05-14 14:55:39 +0200

    feat: Job09 done

---
+++ Jour02/Job09.cpp
@@ -0,0 +1,21 @@
+//
+// Created by porta on 14/05/2025.
+//
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 22, b = 38, c;
+
+        cout << "Entrez un nombre entier, s'il est dans le compris entre les 2 nombres caches vous avez gagne:\n";
+        cin >> c;
+
+        if (c < a || c > b) {
+            cout << "Perdu !" << endl;
+        }
+        else {
+            cout << "Gagne !" << endl;
+            }
+    return 0;
+}
+
