9365a41ebeeee4963ff40339389bd73029a34c40
MrRobot <olivier.portal@laplateforme.io>
2025-05-14 10:50:57 +0200

    feat: Job03 done

--- Jour02/Job03.cpp
+++ Jour02/Job03.cpp
@@ -4,14 +4,26 @@
 #include <iostream>
 using namespace std;
 
+// int main() {
+//     int i = 0, n, sum = 0;
+//     while (i < 4) {
+//         cout << "Donnez un entier: ";
+//         cin >> n;
+//         sum += n;
+//         i++;
+//     }
+//     cout << "Somme: " << sum << endl;
+//     return 0;
+// }
+
 int main() {
     int i = 0, n, sum = 0;
-    while (i < 4) {
+    do {
         cout << "Donnez un entier: ";
         cin >> n;
         sum += n;
         i++;
-    }
+    } while (i < 4);
     cout << "Somme: " << sum << endl;
     return 0;
 }
