a53b36ad97a6ad835265bc45454378acd970894b
MrRobot <olivier.portal@laplateforme.io>
2025-05-16 11:49:25 +0200

    feat: Job02 done

--- Jour03/Job02.cpp
+++ Jour03/Job02.cpp
@@ -1,3 +1,30 @@
 //
 // Created by porta on 16/05/2025.
 //
+#include <iostream>
+#include <cstring>
+
+void supprimeVoyelles(char chaine[]) {
+
+    int i = 0, j = 0;
+
+    while (chaine[i] != '\0') {
+        if (chaine[i] != 'a' && chaine[i] != 'e' && chaine[i] != 'i' &&
+            chaine[i] != 'o' && chaine[i] != 'u' && chaine[i] != 'y') {
+            chaine[j++] = chaine[i];
+        }
+        i++;
+    }
+    chaine[j] = '\0';
+}
+
+int main() {
+    char chaine[100];
+    std::strcpy(chaine, "vive la plateforme !");
+
+    supprimeVoyelles(chaine);
+
+    std::cout << chaine << std::endl;
+
+    return 0;
+}
